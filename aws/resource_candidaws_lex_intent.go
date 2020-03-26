6277b2097202df6b14cadb1a6edc6e40e6b3578f
Beau Bennett <beau.bennett@beauknowssoftware.com>
2020-03-26 18:27:02 -0400

    Full lex intent

--- aws/resource_candidaws_lex_intent.go
+++ aws/resource_candidaws_lex_intent.go
@@ -74,6 +74,132 @@ func resourceAwsLexIntent() *schema.Resource {
 					},
 				},
 			},
+			"slots": {
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"description": {
+							Type:     schema.TypeString,
+							Optional: true,
+							Default:  "Managed by Terraform",
+						},
+						"obfuscation_setting": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"priority": {
+							Type:     schema.TypeInt,
+							Optional: true,
+						},
+						"response_card": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"sample_utterances": {
+							Type:     schema.TypeList,
+							Optional: true,
+							Elem:     &schema.Schema{Type: schema.TypeString},
+						},
+						"slot_constraint": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"slot_type": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"slot_type_version": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"value_elicitation_prompt": {
+							Type:     schema.TypeList,
+							Optional: true,
+							MaxItems: 1,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"messages": {
+										Type:     schema.TypeList,
+										Required: true,
+										MinItems: 1,
+										Elem: &schema.Resource{
+											Schema: map[string]*schema.Schema{
+												"content": {
+													Type:     schema.TypeString,
+													Required: true,
+												},
+												"content_type": {
+													Type:     schema.TypeString,
+													Required: true,
+												},
+												"group_number": {
+													Type:     schema.TypeInt,
+													Optional: true,
+													Default:  1,
+												},
+											},
+										},
+									},
+									"response_card": {
+										Type:     schema.TypeString,
+										Optional: true,
+										Default:  1,
+									},
+									"max_attempts": {
+										Type:     schema.TypeInt,
+										Required: true,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			"confirmation_prompt": {
+				Type:     schema.TypeList,
+				Optional: true,
+				MaxItems: 1,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"messages": {
+							Type:     schema.TypeList,
+							Required: true,
+							MinItems: 1,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"content": {
+										Type:     schema.TypeString,
+										Required: true,
+									},
+									"content_type": {
+										Type:     schema.TypeString,
+										Required: true,
+									},
+									"group_number": {
+										Type:     schema.TypeInt,
+										Optional: true,
+										Default:  1,
+									},
+								},
+							},
+						},
+						"response_card": {
+							Type:     schema.TypeString,
+							Optional: true,
+							Default:  1,
+						},
+						"max_attempts": {
+							Type:     schema.TypeInt,
+							Required: true,
+						},
+					},
+				},
+			},
 			"rejection_statement": {
 				Type:     schema.TypeList,
 				Optional: true,
@@ -110,46 +236,6 @@ func resourceAwsLexIntent() *schema.Resource {
 					},
 				},
 			},
-			"confirmation_prompt": {
-				Type:     schema.TypeList,
-				Optional: true,
-				MaxItems: 1,
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"messages": {
-							Type:     schema.TypeList,
-							Required: true,
-							MinItems: 1,
-							Elem: &schema.Resource{
-								Schema: map[string]*schema.Schema{
-									"content": {
-										Type:     schema.TypeString,
-										Required: true,
-									},
-									"content_type": {
-										Type:     schema.TypeString,
-										Required: true,
-									},
-									"group_number": {
-										Type:     schema.TypeInt,
-										Optional: true,
-										Default: 1,
-									},
-								},
-							},
-						},
-						"response_card": {
-							Type:     schema.TypeString,
-							Optional: true,
-							Default: 1,
-						},
-						"max_attempts": {
-							Type:     schema.TypeInt,
-							Required: true,
-						},
-					},
-				},
-			},
 			"publish": {
 				Type:     schema.TypeBool,
 				Optional: true,
@@ -168,8 +254,9 @@ func resourceAwsLexIntentCreate(d *schema.ResourceData, meta interface{}) error 
 		CreateVersion:       aws.Bool(d.Get("publish").(bool)),
 		SampleUtterances:    expandSampleUtterances(d.Get("sample_utterances").([]interface{})),
 		FulfillmentActivity: expandFulfillmentActivity(d.Get("fulfillment_activity").([]interface{})),
-		ConfirmationPrompt: expandPrompt(d.Get("confirmation_prompt").([]interface{})),
-		RejectionStatement: expandStatement(d.Get("rejection_statement").([]interface{})),
+		ConfirmationPrompt:  expandPrompt(d.Get("confirmation_prompt").([]interface{})),
+		RejectionStatement:  expandStatement(d.Get("rejection_statement").([]interface{})),
+		Slots:               expandSlots(d.Get("slots").([]interface{})),
 	}
 	resp, err := conn.PutIntent(params)
 	if err != nil {
@@ -216,6 +303,10 @@ func resourceAwsLexIntentRead(d *schema.ResourceData, meta interface{}) error {
 		return fmt.Errorf("error setting rejection_statement: %s", err)
 	}
 
+	if err := d.Set("slots", flattenSlots(intent.Slots)); err != nil {
+		return fmt.Errorf("error setting slots: %s", err)
+	}
+
 	return nil
 }
 
@@ -231,8 +322,9 @@ func resourceAwsLexIntentUpdate(d *schema.ResourceData, meta interface{}) error 
 		CreateVersion:       aws.Bool(d.Get("publish").(bool)),
 		SampleUtterances:    expandSampleUtterances(d.Get("sample_utterances").([]interface{})),
 		FulfillmentActivity: expandFulfillmentActivity(d.Get("fulfillment_activity").([]interface{})),
-		ConfirmationPrompt: expandPrompt(d.Get("confirmation_prompt").([]interface{})),
-		RejectionStatement: expandStatement(d.Get("rejection_statement").([]interface{})),
+		ConfirmationPrompt:  expandPrompt(d.Get("confirmation_prompt").([]interface{})),
+		RejectionStatement:  expandStatement(d.Get("rejection_statement").([]interface{})),
+		Slots:               expandSlots(d.Get("slots").([]interface{})),
 	}
 
 	resp, err := conn.PutIntent(params)
@@ -294,8 +386,8 @@ func expandCodeHook(values []interface{}) *lexmodelbuildingservice.CodeHook {
 
 	val := values[0].(map[string]interface{})
 	return &lexmodelbuildingservice.CodeHook{
-		MessageVersion:     aws.String(val["message_version"].(string)),
-		Uri:     aws.String(val["uri"].(string)),
+		MessageVersion: aws.String(val["message_version"].(string)),
+		Uri:            aws.String(val["uri"].(string)),
 	}
 }
 
@@ -306,8 +398,8 @@ func expandPrompt(values []interface{}) *lexmodelbuildingservice.Prompt {
 
 	val := values[0].(map[string]interface{})
 	res := &lexmodelbuildingservice.Prompt{
-		MaxAttempts:     aws.Int64(int64(val["max_attempts"].(int))),
-		Messages:     expandMessages(val["messages"].([]interface{})),
+		MaxAttempts: aws.Int64(int64(val["max_attempts"].(int))),
+		Messages:    expandMessages(val["messages"].([]interface{})),
 	}
 	if responseCard, ok := val["response_card"]; ok {
 		res.ResponseCard = aws.String(responseCard.(string))
@@ -321,7 +413,7 @@ func expandMessages(values []interface{}) []*lexmodelbuildingservice.Message {
 		e := element.(map[string]interface{})
 
 		m := &lexmodelbuildingservice.Message{
-			Content: aws.String(e["content"].(string)),
+			Content:     aws.String(e["content"].(string)),
 			ContentType: aws.String(e["content_type"].(string)),
 		}
 
@@ -342,12 +434,53 @@ func expandStatement(values []interface{}) *lexmodelbuildingservice.Statement {
 
 	val := values[0].(map[string]interface{})
 	res := &lexmodelbuildingservice.Statement{
-		Messages:     expandMessages(val["messages"].([]interface{})),
+		Messages: expandMessages(val["messages"].([]interface{})),
 	}
 	if responseCard, ok := val["response_card"]; ok {
 		res.ResponseCard = aws.String(responseCard.(string))
 	}
 	return res
+}
+
+func expandSlots(values []interface{}) []*lexmodelbuildingservice.Slot {
+	valueSlice := []*lexmodelbuildingservice.Slot{}
+	for _, element := range values {
+		e := element.(map[string]interface{})
+
+		m := &lexmodelbuildingservice.Slot{
+			Name:           aws.String(e["name"].(string)),
+			SlotConstraint: aws.String(e["slot_constraint"].(string)),
+		}
+
+		if description, ok := e["description"]; ok {
+			m.Description = aws.String(description.(string))
+		}
+		if obs, ok := e["obfuscation_setting"]; ok && obs.(string) != "" {
+			m.ObfuscationSetting = aws.String(obs.(string))
+		}
+		if pri, ok := e["priority"]; ok {
+			m.Priority = aws.Int64(int64(pri.(int)))
+		}
+		if rc, ok := e["response_card"]; ok && rc.(string) != "" {
+			m.ResponseCard = aws.String(rc.(string))
+		}
+		if ut, ok := e["sample_utterances"]; ok {
+			m.SampleUtterances = expandStringList(ut.([]interface{}))
+		}
+		if st, ok := e["slot_type"]; ok && st.(string) != "" {
+			m.SlotType = aws.String(st.(string))
+		}
+		if st, ok := e["slot_type_version"]; ok && st.(string) != "" {
+			m.SlotTypeVersion = aws.String(st.(string))
+		}
+		if p, ok := e["value_elicitation_prompt"]; ok {
+			m.ValueElicitationPrompt = expandPrompt(p.([]interface{}))
+		}
+
+		valueSlice = append(valueSlice, m)
+	}
+
+	return valueSlice
 }
 
 func flattenSampleUtterances(cs []*string) []string {
@@ -438,6 +571,44 @@ func flattenStatement(cs *lexmodelbuildingservice.Statement) []map[string]interf
 	}
 
 	return []map[string]interface{}{val}
+}
+
+func flattenSlots(cs []*lexmodelbuildingservice.Slot) []interface{} {
+	valuesSlice := make([]interface{}, len(cs))
+	if len(cs) > 0 {
+		for i, v := range cs {
+			m := make(map[string]interface{})
+			m["name"] = aws.StringValue(v.Name)
+			m["slot_constraint"] = aws.StringValue(v.SlotConstraint)
+			if v.Description != nil {
+				m["description"] = aws.StringValue(v.Description)
+			}
+			if v.ObfuscationSetting != nil {
+				m["obfuscation_setting"] = aws.StringValue(v.ObfuscationSetting)
+			}
+			if v.Priority != nil {
+				m["priority"] = aws.Int64Value(v.Priority)
+			}
+			if v.ResponseCard != nil {
+				m["response_card"] = aws.StringValue(v.ResponseCard)
+			}
+			if v.SampleUtterances != nil {
+				m["sample_utterances"] = flattenStringList(v.SampleUtterances)
+			}
+			if v.SlotType != nil {
+				m["slot_type"] = aws.StringValue(v.SlotType)
+			}
+			if v.SlotTypeVersion != nil {
+				m["slot_type_version"] = aws.StringValue(v.SlotTypeVersion)
+			}
+			if v.ValueElicitationPrompt != nil {
+				m["value_elicitation_prompt"] = flattenPrompt(v.ValueElicitationPrompt)
+			}
+			valuesSlice[i] = m
+		}
+	}
+
+	return valuesSlice
 }
 
 func getLexIntent(name, version string, conn *lexmodelbuildingservice.LexModelBuildingService) (*lexmodelbuildingservice.GetIntentOutput, error) {

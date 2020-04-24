7ad7bae60b7fd3e370acbc00cb4526bde791df3c
ebower12 <ethan.bower@candidpartners.com>
2020-04-24 09:50:06 -0400

    added computed properties

--- aws/resource_candidaws_govcloud_account.go
+++ aws/resource_candidaws_govcloud_account.go
@@ -2,7 +2,9 @@ package aws
 
 import (
 	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 	"log"
+	"regexp"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -28,29 +30,47 @@ func resourceAwsGovcloudAccount() *schema.Resource {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
-			"account_name": {
+			"joined_method": {
 				Type:     schema.TypeString,
-				Required: true,
-				ForceNew: true,
+				Computed: true,
+			},
+			"joined_timestamp": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"parent_id": {
+				Type:         schema.TypeString,
+				Computed:     true,
+				Optional:     true,
+				ValidateFunc: validation.StringMatch(regexp.MustCompile("^(r-[0-9a-z]{4,32})|(ou-[0-9a-z]{4,32}-[a-z0-9]{8,32})$"), "see https://docs.aws.amazon.com/organizations/latest/APIReference/API_MoveAccount.html#organizations-MoveAccount-request-DestinationParentId"),
+			},
+			"status": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"account_name": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.StringLenBetween(1, 50),
 			},
 			"email": {
-				Type:     schema.TypeString,
-				Required: true,
-				ForceNew: true,
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validateAwsOrganizationsAccountEmail,
 			},
 			"iam_user_access_to_billing": {
-				Type:     schema.TypeString,
-				Required: true,
-				ForceNew: false,
-				ExactlyOneOf: []string{
-					"ALLOW",
-					"DENY",
-				},
+				Type:         schema.TypeString,
+				Optional:     true,
+				ForceNew:     false,
+				ValidateFunc: validation.StringInSlice([]string{organizations.IAMUserAccessToBillingAllow, organizations.IAMUserAccessToBillingDeny}, true),
 			},
 			"role_name": {
-				Type:     schema.TypeString,
-				Optional: true,
-				ForceNew: true,
+				Type:         schema.TypeString,
+				Optional:     true,
+				ForceNew:     true,
+				ValidateFunc: validateAwsOrganizationsAccountRoleName,
 			},
 			"tags": tagsSchema(),
 		},
@@ -61,13 +81,14 @@ func resourceAwsGovcloudAccountCreate(d *schema.ResourceData, meta interface{}) 
 	conn := meta.(*AWSClient).organizationsconn
 
 	params := &organizations.CreateGovCloudAccountInput{
-		AccountName:            aws.String(d.Get("account_name").(string)),
-		Email:                  aws.String(d.Get("email").(string)),
-		IamUserAccessToBilling: aws.String(d.Get("iam_user_access_to_billing").(string)),
-		RoleName:               aws.String(d.Get("role_name").(string)),
+		AccountName: aws.String(d.Get("account_name").(string)),
+		Email:       aws.String(d.Get("email").(string)),
 	}
 	if role, ok := d.GetOk("role_name"); ok {
 		params.RoleName = aws.String(role.(string))
+	}
+	if iam_user, ok := d.GetOk("iam_user_access_to_billing"); ok {
+		params.IamUserAccessToBilling = aws.String(iam_user.(string))
 	}
 
 	log.Printf("[DEBUG] Creating AWS GovCloud Accounnt: %s", params)
@@ -112,13 +133,35 @@ func resourceAwsGovcloudAccountCreate(d *schema.ResourceData, meta interface{}) 
 	stateResp, stateErr := stateConf.WaitForState()
 	if stateErr != nil {
 		return fmt.Errorf(
-			"Error waiting for account request (%s) to become available: %s",
+			"error waiting for account request (%s) to become available: %s",
 			requestID, stateErr)
 	}
 
 	// Store the ID
 	accountID := stateResp.(*organizations.CreateAccountStatus).GovCloudAccountId
 	d.SetId(*accountID)
+
+	if v, ok := d.GetOk("parent_id"); ok {
+		newParentID := v.(string)
+
+		existingParentID, err := resourceAwsOrganizationsAccountGetParentId(conn, d.Id())
+
+		if err != nil {
+			return fmt.Errorf("error getting AWS Organizations Account (%s) parent: %s", d.Id(), err)
+		}
+
+		if newParentID != existingParentID {
+			input := &organizations.MoveAccountInput{
+				AccountId:           accountID,
+				SourceParentId:      aws.String(existingParentID),
+				DestinationParentId: aws.String(newParentID),
+			}
+
+			if _, err := conn.MoveAccount(input); err != nil {
+				return fmt.Errorf("error moving AWS Organizations Account (%s): %s", d.Id(), err)
+			}
+		}
+	}
 
 	if v := d.Get("tags").(map[string]interface{}); len(v) > 0 {
 		if err := keyvaluetags.OrganizationsUpdateTags(conn, d.Id(), nil, v); err != nil {
@@ -159,6 +202,8 @@ func resourceAwsGovcloudAccountRead(d *schema.ResourceData, meta interface{}) er
 	}
 
 	d.Set("arn", account.Arn)
+	d.Set("joined_method", account.JoinedMethod)
+	d.Set("joined_timestamp", aws.TimeValue(account.JoinedTimestamp).Format(time.RFC3339))
 	d.Set("account_name", account.Name)
 	d.Set("email", account.Email)
 	d.Set("parent_id", parentID)

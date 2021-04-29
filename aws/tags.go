967993c58b179cd4b626e651d13967a8cfda8c7c
Kaleb Wade <kaleb.wade@mckinsey.com>
2021-04-29 11:25:25 -0500

    Add SetTagsDiff function, however it is not impletmented yet

--- aws/tags.go
+++ aws/tags.go
@@ -1,7 +1,11 @@
 package aws
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/terraform-providers/terraform-provider-aws/aws/internal/keyvaluetags"
 )
 
 // tagsSchema returns the schema to use for tags.
@@ -19,4 +23,29 @@ func tagsSchemaComputed() *schema.Schema {
 		Computed: true,
 		Elem:     &schema.Schema{Type: schema.TypeString},
 	}
+}
+
+// SetTagsDiff sets the new plan difference with the result of
+// merging resource tags on to those defined at the provider-level;
+// returns an error if unsuccessful or if the resource tags are identical
+// to those configured at the provider-level to avoid non-empty plans
+// after resource READ operations as resource and provider-level tags
+// will be indistinguishable when returned from an AWS API.
+func SetTagsDiff(_ context.Context, diff *schema.ResourceDiff, meta interface{}) error {
+	defaultTagsConfig := meta.(*AWSClient).DefaultTagsConfig
+	ignoreTagsConfig := meta.(*AWSClient).IgnoreTagsConfig
+
+	resourceTags := keyvaluetags.New(diff.Get("tags").(map[string]interface{}))
+
+	if defaultTagsConfig.TagsEqual(resourceTags) {
+		return fmt.Errorf(`"tags" are identical to those in the "default_tags" configuration block of the provider: please de-duplicate and try again`)
+	}
+
+	allTags := defaultTagsConfig.MergeTags(resourceTags).IgnoreConfig(ignoreTagsConfig)
+
+	if err := diff.SetNew("tags_all", allTags.Map()); err != nil {
+		return fmt.Errorf("error setting new tags_all diff: %w", err)
+	}
+
+	return nil
 }
